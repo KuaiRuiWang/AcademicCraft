d4e0db382ef5c53bd05959981cde116dc2065e0f
ThyShy <nineteen@futurevita.it.com>
2026-06-07 04:34:12 +0800

    feat(app): 添加应用管理和代码生成功能
    - 在AiCodeGeneratorFacade中增加appId参数支持
    - 添加AppConstant常量定义应用相关配置
    - 新增AppService接口和AppServiceImpl实现类
    - 添加App实体类和相关DTO、VO数据传输对象
    - 实现AppController提供应用CRUD和代码生成接口
    - 添加StaticResourceController支持静态资源访问
    - 修改CodeFileSaverExecutor和CodeFileSaverTemplate支持按appId保存代码
    - 实现应用代码部署和预览功能

---
+++ src/main/java/com/wkr/aicode/mapper/AppMapper.java
@@ -0,0 +1,13 @@
+package com.wkr.aicode.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.wkr.aicode.model.entity.App;
+
+/**
+ * 应用 映射层。
+ *
+ * @author <a>百夜</a>
+ */
+public interface AppMapper extends BaseMapper<App> {
+
+}
---
+++ src/main/java/com/wkr/aicode/service/AppService.java
@@ -0,0 +1,31 @@
+package com.wkr.aicode.service;
+
+import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.core.service.IService;
+import com.wkr.aicode.model.dto.app.AppQueryRequest;
+import com.wkr.aicode.model.entity.App;
+import com.wkr.aicode.model.entity.User;
+import com.wkr.aicode.model.vo.app.AppVO;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+
+/**
+ * 应用 服务层。
+ *
+ * @author <a>百夜</a>
+ */
+public interface AppService extends IService<App> {
+
+    AppVO getAppVO(App app);
+
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+
+    List<AppVO> getAppVOList(List<App> appList);
+
+    Flux<String> chatToGenCode(Long appId, String message, User loginUser);
+
+    String deployApp(Long appId, User loginUser);
+
+}
