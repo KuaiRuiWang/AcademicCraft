e93d00f8bc1741d9d05ac780136b2c75765a7ec9
ThyShy <nineteen@futurevita.it.com>
2026-06-10 19:11:11 +0800

    chore(frontend): 重构前端

--- ai-code-fronten/openapi2ts.config.ts
+++
@@ -1,6 +0,0 @@
-// 根据后端代码生成前端请求代码
-export default {
-  requestLibPath: "import request from '@/request'",
-  schemaPath: 'http://localhost:8123/api/v3/api-docs',
-  serversPath: './src',
-}
--- ai-code-fronten/src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import UserManagePage from '@/views/admin/UserManagePage.vue'
-</script>
-
-<template>
-  <main>
-    首页
-  </main>
-</template>
